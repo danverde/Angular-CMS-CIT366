81a12fab4ae627ca00e432a2315e2701aad6081c
Danverde <gre12014@byui.edu>
2017-11-09 11:17:22 -0700

    made it to creating updateDocument function

--- project/src/app/documents/document-list/document-list.component.ts
+++ project/src/app/documents/document-list/document-list.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Document } from '../document.model';
 import { DocumentService } from '../document.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
-export class DocumentListComponent implements OnInit {
+export class DocumentListComponent implements OnInit, OnDestroy {
    documents: Document[] = [];
+   private subscription: Subscription;
 
    constructor(private documentService: DocumentService) {
    }
 
    ngOnInit() {
      this.documents = this.documentService.getDocuments();
-     this.documentService.documentChangedEvent
+     this.documentService.documentListChangedEvent
       .subscribe((documents: Document[]) => {
         this.documents = documents;
       });
+   }
+
+   ngOnDestroy() {
+this.subscription.unsubscribe();
    }
 }
--- project/src/app/documents/document.service.ts
+++ project/src/app/documents/document.service.ts
@@ -1,14 +1,19 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Document } from './document.model';
 import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class DocumentService {
    documentSelectedEvent = new EventEmitter<Document>();
    documentChangedEvent = new EventEmitter<Document[]>();
+   documentListChangedEvent = new Subject<Document[]>();
    documents: Document[] = [];
+   maxDocumentId: number;
+
    constructor() {
       this.documents = MOCKDOCUMENTS;
+      this.maxDocumentId = this.getMaxId();
    }
 
    getDocuments() {
@@ -25,7 +30,28 @@ export class DocumentService {
       return doc;
    }
 
-   deleteDocument(document: Document) {
+   getMaxId (): number {
+    let maxId = 0;
+    this.documents.forEach((document: Document) => {
+        const currId = +document.id;
+        if (currId > maxId) {
+            maxId = currId;
+        }
+    });
+    return maxId;
+   }
+
+   addDocument (newDoc: Document) {
+    if (newDoc === undefined || newDoc === null){
+        return;
+    }
+    this.maxDocumentId++;
+    newDoc.id = this.maxDocumentId.toString();
+    this.documents.push(newDoc);
+    this.documentListChangedEvent.next([...this.documents]);
+   }
+
+    deleteDocument(document: Document) {
     if (document === null) {
         return;
     }
