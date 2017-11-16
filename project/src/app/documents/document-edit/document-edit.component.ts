f5b9e7f1ea8d90b003b111fc39ce46298f238fdc
Danverde <gre12014@byui.edu>
2017-11-16 10:13:29 -0700

    completed document add/edit form

--- project/src/app/documents/document-edit/document-edit.component.ts
+++ project/src/app/documents/document-edit/document-edit.component.ts
@@ -2,6 +2,7 @@ import { Document } from './../document.model';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { DocumentService } from './../document.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms/src/directives/ng_form';
 
 @Component({
   selector: 'app-document-edit',
@@ -16,6 +17,24 @@ export class DocumentEditComponent implements OnInit {
   constructor(private documentService: DocumentService,
     private router: Router,
     private route: ActivatedRoute) { }
+
+
+    onSubmit(form: NgForm) {
+      const values = form.value;
+
+      const newDoc = new Document(null, values.name, values.description, values.docUrl, null);
+
+      if (this.editMode === true) {
+        this.documentService.updateDocument(this.originalDocument, newDoc);
+      } else {
+        this.documentService.addDocument(newDoc);
+      }
+      this.router.navigate(['/documents'], {relativeTo: this.route});
+    }
+
+    onCancel() {
+      this.router.navigate(['/documents'], {relativeTo: this.route});
+    }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
