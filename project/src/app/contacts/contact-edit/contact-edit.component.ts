5bdc366b70ed81a73403a144d252dae9e58d9898
Danverde <gre12014@byui.edu>
2017-11-16 11:48:37 -0700

    fixed issues with contactForm. now working

--- project/src/app/contacts/contact-edit/contact-edit.component.ts
+++ project/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,4 +1,8 @@
+import { Router, Params, ActivatedRoute } from '@angular/router';
+import { ContactService } from './../contact.service';
+import { Contact } from './../contacts.model';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms/src/directives/ng_form';
 
 @Component({
   selector: 'app-contact-edit',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-edit.component.css']
 })
 export class ContactEditComponent implements OnInit {
+  originalContact: Contact;
+  contact: Contact = null;
+  groupContacts: Contact[] = [];
+  editMode = false;
+  hasGroup = false;
 
-  constructor() { }
+  constructor(private contactService: ContactService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  onSubmit(form: NgForm) {
+    const values = form.value;
+
+    const newContact = new Contact(null, values.name, values.email, values.phone, values.imageUrl, null);
+
+    if (this.editMode === true) {
+      this.contactService.updateContact(this.originalContact, newContact);
+    } else {
+      this.contactService.addContact(newContact);
+    }
+
+    this.router.navigate(['/contacts'], {relativeTo: this.route});
   }
 
+  onCancel() {
+    this.router.navigate(['/contacts'], {relativeTo: this.route});
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
+
+      if (id === undefined || id === null) {
+        this.editMode = false;
+        return;
+      }
+      this.originalContact = this.contactService.getContact(id);
+      if (this.originalContact === null || this.originalContact === undefined) {
+        return;
+      }
+
+      this.editMode = true;
+      this.contact = JSON.parse(JSON.stringify(this.originalContact));
+      if (this.contact.group !== null && this.contact.group !== undefined) {
+        // this.groupContacts = [...this.contact.group];
+        console.log(this.contact);
+      }
+    });
+  }
 }
--- project/src/app/contacts/contact-list/contact-list.component.ts
+++ project/src/app/contacts/contact-list/contact-list.component.ts
@@ -17,7 +17,7 @@ export class ContactListComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.contacts = this.contactService.getContacts();
-    this.contactService.contactListChangedEvent
+    this.subscription = this.contactService.contactListChangedEvent
     .subscribe((contacts: Contact[]) => {
       this.contacts = contacts;
     });
